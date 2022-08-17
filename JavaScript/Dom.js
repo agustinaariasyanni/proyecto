48fe3a565e929b8bace29950389e58fbc837552c
AgustinaAY <agustinaariasyanni@gmail.com>
2022-08-16 21:02:28 -0300

    dom

---
+++ JavaScript/Dom.js
@@ -0,0 +1,12 @@
+const titulo = document.getElementById("titulo")
+const parrafo = document.getElementById("parrafo")
+
+const botonRosa = document.getElementsByClassName("button-grey")
+const botones = document.getElementsByClassName("button")
+
+const buttons = document.getElementsByTagName("button")
+
+function textoDeBotones() {
+    for (let boton of boton)
+   console.log(button.innerText)
+}
